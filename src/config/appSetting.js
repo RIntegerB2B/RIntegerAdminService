36e3ebd976b98a0f6ef7c4288ea27e8be2d27bfb
Chandran <Preveen_Chandran@Dell.com>
2018-08-14 14:05:36 +0530

    Setting

--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -2,5 +2,5 @@ module.exports = {
     // imageServerPath: 'http://localhost/RInteger/serviceproviders/',
     imageServerPath: 'https://rinteger.com/admin/images/',
     //imageUploadPath: './models/serviceproviders/'
-    imageUploadPath:'/home/ubuntu/app/rinteger-admin/images'
+    imageUploadPath:'/home/ubuntu/app/rinteger-admin/images/'
  }
