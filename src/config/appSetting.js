b4a4e52b7713643056be9450321b3f697f15730b
Vikasini <vikasininithi@gmail.com>
2018-10-25 12:12:57 +0530

    changes in notification

--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -1,8 +1,8 @@
 module.exports = {
-    imageServerPath: 'http://localhost/RInteger/serviceproviders/',
-       imageUploadPath: './models/serviceproviders/', 
-/*    imageServerPath: process.env.IMAGE_SERVER_PATH || 'https://rinteger.com/admin/images/',
- imageUploadPath:process.env.IMAGE_UPLOAD_PATH || '/home/ubuntu/app/rinteger-admin/Rinteger-admin/images/' ,*/
+/*   imageServerPath: 'http://localhost/RInteger/serviceproviders/',
+       imageUploadPath: './models/serviceproviders/', */
+   imageServerPath: process.env.IMAGE_SERVER_PATH || 'https://rinteger.com/admin/images/',
+ imageUploadPath:process.env.IMAGE_UPLOAD_PATH || '/home/ubuntu/app/rinteger-admin/Rinteger-admin/images/' ,
    imageUrl: process.env.IMAGE_URL_PATH || 'https://rinteger.com/assets/images/logohomepage.jpg',
-   serviceUrl: 'https://rinteger.com/',
+   serviceUrl: 'https://rinteger.com/dashboard/statusView/',
  }
