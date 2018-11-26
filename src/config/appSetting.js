004ec7539125e735d0b7aa79e6890f5225b9f00d
ripsildeveloper <ripsildeveloper3@gmail.com>
2018-11-26 23:13:25 +0530

    changes image url

--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -1,9 +1,9 @@
 module.exports = {
-imageServerPath: 'http://localhost/RInteger/serviceproviders/',
-        imageUploadPath: './models/serviceproviders/', 
-   /* imageServerPath: process.env.IMAGE_SERVER_PATH || 'https://rinteger.com/admin/images/',
+/*  imageServerPath: 'http://localhost/RInteger/serviceproviders/',
+        imageUploadPath: './models/serviceproviders/', */
+   imageServerPath: process.env.IMAGE_SERVER_PATH || 'https://rinteger.com/admin/images/',
   imageUploadPath:process.env.IMAGE_UPLOAD_PATH || '/home/ubuntu/app/rinteger-admin/Rinteger-admin/images/' ,
-    */
+    
     /* workServerPath: 'http://localhost/RIntegerWorks/',
     workUplaodPath: './ourwork/', */
     imageUrl: process.env.IMAGE_URL_PATH || 'https://rinteger.com/assets/images/logohomepage.jpg',
