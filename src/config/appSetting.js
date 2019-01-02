efb7f01dbe0502ef9a16314584d19fdc67f94be7
Vikasini <vikasininithi@gmail.com>
2019-01-02 17:39:33 +0530

    changes in config

--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -6,8 +6,8 @@ module.exports = {
   imageOurWorkUploadPath: './ourwork/', */
 
 
-  adServerPath: 'http://localhost/RIntegerAds',
- adUploadPath: './ads', 
+ /*  adServerPath: 'http://localhost/RIntegerAds',
+ adUploadPath: './ads',  */
 
    imageServerPath: process.env.IMAGE_SERVER_PATH || 'https://rinteger.com/admin/images/',
   imageUploadPath:process.env.IMAGE_UPLOAD_PATH || '/home/ubuntu/app/rinteger-admin/Rinteger-admin/images/' ,
@@ -16,8 +16,8 @@ module.exports = {
     imageOurWorkServerPath: process.env.IMAGE_SERVER_PATH || 'https://rinteger.com/admin/images/ourwork/',
     imageOurWorkUploadPath:process.env.IMAGE_UPLOAD_PATH || '/home/ubuntu/rinteger-admin/Rinteger-admin/images/ourwork/' ,
 
- /*    adServerPath:  'https://rinteger.com/admin/images/ads/',
- adUploadPath: '/home/ubuntu/app/rinteger-admin/Rinteger-admin/images/ads/' , */
+    adServerPath:  'https://rinteger.com/admin/images/ads/',
+ adUploadPath: '/home/ubuntu/app/rinteger-admin/Rinteger-admin/images/ads/' ,
     imageUrl: process.env.IMAGE_URL_PATH || 'https://rinteger.com/assets/images/logohomepage.jpg',
     serviceUrl: 'https://rinteger.com/dashboard/statusView/',
     notificationUrl: 'https://rinteger.com/admin/navheader/crmsubscribe'
