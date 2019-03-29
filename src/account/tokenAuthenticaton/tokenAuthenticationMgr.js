f43f25213ae3349b1c90117644b08e970f71518e
Chandran, Preveen <preveen_chandran@dell.com>
2019-03-29 09:53:30 +0530

    Enabled Token Auth

--- src/account/tokenAuthenticaton/tokenAuthenticationMgr.js
+++ src/account/tokenAuthenticaton/tokenAuthenticationMgr.js
@@ -3,29 +3,34 @@ var jwt = require('jsonwebtoken');
 var AdminAccount = require('./../../model/adminAccount.model')
 exports.validateToken = function (req, res, next) {
   console.log('LOGGED');
+  var allowAnonymousRoute = false;
   var secretKey = '123456789?*!^sghd';
   var token = req.headers['authorization'];
   /* eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFkbWluIiwiaWF0IjoxNTUxMTYwNzkzLCJleHAiOjE1NTEyMDM5OTN9.sDiOpMgMTexvLeDiyxZGsZXgIJVdYrqg8VfCdS0kpl0 */
   var allowAnonymousRoutes = ['/test', '/validate', '/pushnotificationsubscribe', 'some other route'];
-
+  
   for (var i = 0; i < allowAnonymousRoutes.length; i++) {
     console.log(allowAnonymousRoutes[i] === req.path);
-    if (allowAnonymousRoutes[i] === req.path) {
-      console.log(allowAnonymousRoutes[i]);
-      next();
-    } else {
-      console.log(allowAnonymousRoutes[i]);
-      next();
-      /* jwt.verify(token, secretKey, function (err, userName) {
+    if (allowAnonymousRoutes[i] === req.path){
+      allowAnonymousRoute=true;
+      break;
+    }
+  }
+  if (allowAnonymousRoute) {
+    console.log('anonymous route');
+    next();
+  } else {
+    if (token)   {
+      jwt.verify(token, secretKey, function (err, userName) {
         if (err)
           return res.status(401).send({
             auth: false,
             message: 'Failed to authenticate token.'
           });
         else {
-
+  
           //Check userName is available in MongoDB, if it is there next() , or return res.status(401).send({       auth: false,        message: 'Token is invalid'    });
-
+  
           AdminAccount.findOne({
             'userName': userName.id
           }, function (err, userDetail) {
@@ -34,17 +39,25 @@ exports.validateToken = function (req, res, next) {
                 message: "Some error occurred while retrieving notes."
               });
             } else {
-              if (!userDetail) 
-              return res.status(401).send({
-                auth: false,
-                message: 'not find'
-              });
-              next();
+              if (!userDetail) {
+                return res.status(401).send({
+                  auth: false,
+                  message: 'not find'
+                });
+              }else{
+                next();
+              }
             }
           });
         }
-      }); */
+      }); 
     }
-  };
-
-}
+    else{
+      return res.status(401).send({
+        auth: false,
+        message: 'Failed to authenticate token.'
+      });
+    }
+     
+  }
+};
--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({
 app.use(cors());
 
 routes.loadRoutes(app);
-/* app.use(tokenAuthMgr.validateToken); */
+app.use(tokenAuthMgr.validateToken);
 app.listen(port);
 
 
